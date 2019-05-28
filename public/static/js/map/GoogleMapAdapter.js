fb147365d6144e5c2166e504ce4d3d4fbeef023d
Denis <you@example.com>
2019-05-29 00:32:12 +1000

    Гугл тоже строит полилайн

--- public/static/js/map/GoogleMapAdapter.js
+++ public/static/js/map/GoogleMapAdapter.js
@@ -41,4 +41,18 @@ class GoogleMapAdapter extends AbstractMapAdapter {
 	setCenter([lat, lon]) {
 		this.map.setCenter({lat, lng: lon});
 	}
+
+	addPolyline(coordinatesArray, style) {
+		var flightPlanCoordinates = coordinatesArray.map(([lat, lng]) => ({lat, lng}));
+
+		var flightPath = new google.maps.Polyline({
+			path: flightPlanCoordinates,
+			geodesic: true,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 2
+		});
+
+		flightPath.setMap(this.map);
+	}
 }
