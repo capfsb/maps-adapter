04c48d45bb5669adbd2f11a70c690e29e78057de
Denis <you@example.com>
2019-05-30 04:05:28 +1000

    Подсветка точки при наведении на булетин, только в яндекс карте

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -46,17 +46,27 @@ $(async () => {
 	/** Парсим точки из булетинов и оставляем, только уникальные, добавляем их на карту */
 	let allPoints = $('[data-coordinates]').get().map(value => $(value).attr('data-coordinates'));
 	let uniquePoints = [...new Set(allPoints)];
+	let mapCoordinatesToPointId = {};
 
 	uniquePoints.forEach(pointStr => {
 		let point = adapter.addPoint(JSON.parse(pointStr));
+		mapCoordinatesToPointId[pointStr] = point.getId();
 
-		point.onMouseIn(()=>{
+		point.onMouseIn(() => {
 			$(`[data-coordinates="${pointStr}"]`).css('background-color', 'green')
 		});
 
 		point.onMouseOut(()=>{
 			$(`[data-coordinates="${pointStr}"]`).css('background-color', '')
 		});
+	});
+
+	$('[data-coordinates]').on('mouseenter', e => {
+		let id = mapCoordinatesToPointId[$(e.currentTarget).attr('data-coordinates')];
+		adapter.findGeoObject(id).setActive(true);
+	}).on('mouseleave', e => {
+		let id = mapCoordinatesToPointId[$(e.currentTarget).attr('data-coordinates')];
+		adapter.findGeoObject(id).setActive(false);
 	})
 
 });
